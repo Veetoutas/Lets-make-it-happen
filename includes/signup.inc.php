da5b23bd29509b3d9dad010345ae91d4c8454415
Veetoutas <vyt.matulevicius@gmail.com>
2020-07-30 07:35:57 +0300

    Signup bug fixed. Same users were able to signup v.4

--- includes/signup.inc.php
+++ includes/signup.inc.php
@@ -42,11 +42,11 @@ if (isset($_POST['signup-submit'])) {
         $stmt = mysqli_stmt_init($conn);
         if (!mysqli_stmt_prepare($stmt, $sql)) {
             header('Location: signup.php?error=sqlerror');
-//            die('cia');
             exit();
         }
         else  {
             mysqli_stmt_bind_param($stmt, 'ss', $username, $email);
+            mysqli_stmt_execute($stmt);
             mysqli_stmt_store_result($stmt);
             $resultCheck = mysqli_stmt_num_rows($stmt);
             if ($resultCheck > 0) {
