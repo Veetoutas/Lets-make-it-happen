3714b7f033de4f113275394a95291d774cf74e31
Veetoutas <vyt.matulevicius@gmail.com>
2020-08-03 13:57:33 +0300

    Commiting files to branch

---
+++ includes/login.inc.php
@@ -0,0 +1,68 @@
+<?php
+
+
+if (isset($_POST['login-submit'])) {
+
+    require 'db.inc.php';
+
+    $username = $_POST['username'];
+    $pwd = $_POST['pwd'];
+
+    // We check to see if the users inputs are spelled correctly and check for empty fields
+    if (empty($username || $pwd)) {
+        header('Location: ../login.php?error=emptyfields');
+        exit();
+    }
+
+    // Now we check if a user already exists with that username or e-mail
+    else {
+
+        $sql = "SELECT * FROM users WHERE nameUsers=? OR emailUsers=?";
+        $stmt = mysqli_stmt_init($conn);
+        if (!mysqli_stmt_prepare($stmt, $sql)) {
+            header('Location: ../login.php?error=sqlerror');
+            exit();
+        }
+        else {
+            mysqli_stmt_bind_param($stmt, 'ss', $username, $username);
+            mysqli_stmt_execute($stmt);
+            $result = mysqli_stmt_get_result($stmt);
+
+            if ($row = mysqli_fetch_assoc($result)) {
+                $pwdCheck = password_verify($pwd, $row['pwdUsers']);
+                if($pwdCheck == false) {
+                    header('Location: ../login.php?error=wrongpwd');
+                    exit();
+                }
+
+                else if ($pwdCheck == true) {
+                    session_start();
+                    $_SESSION['userId'] = $row['idUsers'];
+                    $_SESSION['userName'] = $row['nameUsers'];
+                    $_SESSION['userRole'] = $row['role'];
+                    header('Location: ../index.php?login=success');
+                    exit();
+                }
+
+            }
+
+            else {
+                header('Location: ../index.php?error=wrongpwd');
+                exit();
+            }
+
+        }
+    }
+
+}
+
+// Just in case a hacker managed to go through validation, redirect him to index.php
+else {
+
+    header('Location: ../index.php?');
+    exit();
+
+}
+
+
+
---
+++ signup.php
@@ -0,0 +1,74 @@
+<?php
+    require 'header.php';
+?>
+<div class="container">
+    <div class="d-flex justify-content-center h-100">
+        <div class="card">
+            <div class="card-header">
+                <h3>Sign Up</h3>
+                <div id="error_msg">
+                    <?php
+                        include 'includes/errors.inc.php';
+                    ?>
+                </div>
+            </div>
+            <div class="card-body">
+                <form method="POST" action="includes/signup.inc.php">
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="text" name="username" class="form-control" placeholder="Username">
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-user"></i></span>
+                        </div>
+                        <input type="email" name="email" class="form-control" placeholder="E-mail">
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password" name="pwd" class="form-control" placeholder="Password">
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fas fa-key"></i></span>
+                        </div>
+                        <input type="password" name="repeat-pwd" class="form-control" placeholder="Repeat-password">
+                    </div>
+
+                    <div class="input-group form-group">
+                        <div class="input-group-prepend">
+                            <span class="input-group-text"><i class="fab fa-black-tie"></i></span>
+                        </div>
+                        <select name="role" class="form-control" required>
+                            <option value="empty_role_value" disabled selected hidden>Choose a role</option>
+                            <option >Editor</option>
+                            <option>Admin</option>
+                        </select>
+                    </div>
+
+                    <div class="form-group">
+                        <button  type="submit" name="signup-submit" class="btn float-right login_btn">Sign Up</button>
+                    </div>
+
+                </form>
+            </div>
+            <div class="card-footer">
+                <div class="d-flex justify-content-center links">
+                    Already a member?<a href="login.php">Log in!</a>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php
+    require 'footer.php';
+?>
